a4cd2c70e84a93dbf26d3f15eb8ef074ea3c025b
Louis J <louis.bs.jeanneau@gmail.com>
2023-12-13 21:22:16 +0100

    2023 day 12 part 1 heuristic

--- Python/2023/2023_day12.py
+++ Python/2023/2023_day12.py
@@ -1,3 +1,4 @@
+import itertools
 import re
 
 import constraint as cstt
@@ -11,44 +12,25 @@ with open("2023_day12_sample.txt") as f:
 print("input = ", data)
 
 # Part 1
+total = 0
 for line in data:
-    print(line)
+    # print(line)
     puzzle, hint = line.split(" ")
     unknowns = [ind for ind, c in enumerate(puzzle) if c == "?"]
     dots = [ind for ind, c in enumerate(puzzle) if c == "."]
     hashtags = [ind for ind, c in enumerate(puzzle) if c == "#"]
     hint = [int(i) for i in re.findall(r'\d+', hint)]
-    sum_hint = sum(hint)
-    problem = cstt.Problem()
-    for i in range(len(puzzle)):
-        problem.addVariable(i, [0, 1])
-    problem.addConstraint(cstt.ExactSumConstraint(sum_hint))
-    for i in dots:
-        problem.addConstraint(cstt.ExactSumConstraint(0), [i])
-    for i in hashtags:
-        problem.addConstraint(cstt.ExactSumConstraint(1), [i])
-    solutions = problem.getSolutions()
-    for ind in range(len(hint)):
-        valueRight = sum(hint[ind + 1:])
-        valueLeft = sum(hint[:ind + 1])
-        rightPart = cstt.MinSumConstraint(valueRight)
-        rightRange = [i for i in range(valueLeft + ind + 1, len(puzzle))]
-        if len(rightRange):
-            problem.addConstraint(rightPart, rightRange)
-        solutions = problem.getSolutions()
-
-        leftPart = cstt.MaxSumConstraint(valueLeft)
-        leftRange = [i for i in range(valueLeft + ind)]
-        if len(leftRange):
-            problem.addConstraint(leftPart, leftRange)
-        solutions = problem.getSolutions()
-
-
-    solutions = problem.getSolutions()
-    print(len(solutions))
-
-
-
-
+    sumHint = sum(hint)
+    sumHash = len(hashtags)
+    for combi in list(itertools.product([0, 1], repeat=len(unknowns))):
+        if sum(combi) + sumHash != sumHint:
+            continue
+        copyLine = list(line)
+        for index, value in enumerate(unknowns):
+            copyLine[value] = '#' if combi[index]==1 else '.'
+        groups = re.findall(r'#+', ''.join(copyLine))
+        if [len(g) for g in groups] == hint:
+            total += 1
+print("total = ", total)
 
 # Part 2
