24fa6c31364ac9d16442e6970b069cf79528869f
Louis J <louis.bs.jeanneau@gmail.com>
2023-12-17 23:53:02 +0100

    2023 day 14 part 1

--- Python/2023/2023_day14.py
+++ Python/2023/2023_day14.py
@@ -1,9 +1,10 @@
+import itertools
 import re
 import numpy as np
 
-# with open("2023_day14_input.txt") as f:
-with open("2023_day14_sample.txt") as f:
-   data = [list(l.strip()) for l in f.readlines()]
+with open("2023_day14_input.txt") as f:
+# with open("2023_day14_sample.txt") as f:
+    data = [list(l.strip()) for l in f.readlines()]
 
 # data = open('2023_day14_sample.txt', 'r').read()
 
@@ -14,16 +15,25 @@ print("Part 1")
 map = np.array(data)
 map = map.T
 cubes = np.where(map == '#')
-l = len(map)
-cubesPositions = [(i, l+1) for i in range(l)]
-cubesPositions.extend(list(zip(cubes[0], cubes[1])))
-seen = set()
+cubesPositions = list(zip(cubes[0], cubes[1]))
+cubesPositions.extend([(i, -1) for i in range(len(map))])
+cubesPositions.extend([(i, len(map[0])) for i in range(len(map))])
 cubesPositions = sorted(cubesPositions, key=lambda x: (x[0], x[1]))
-cubesPositions = [y for x,y in cubesPositions if not x in seen and not seen.add(x)]
-rollingStones = []
+l = len(map[0])
+total = 0
 for x in range(len(map)):
-   rollingStones.append(sum([len(i) for i in re.findall(r'O+', ''.join(map[x])[:cubesPositions[x]])]))
-print(sum([sum([j for j in range(l,l-i, -1)]) for i in rollingStones]))
+    lastCube = -1
+    seenRound = 0
+    for index, c in enumerate(''.join(map[x])):
+        if c == "#":
+            total += sum(range(l-lastCube-1, l-lastCube-1-seenRound, -1))
+            lastCube = index
+            seenRound = 0
+        elif c == "O":
+            seenRound += 1
+        if index == l-1:
+            total += sum(range(l-lastCube-1, l-lastCube-1-seenRound, -1))
 
+print("total = ", total)
 
 # Part 2
