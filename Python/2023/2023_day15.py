3f59d742d9cec08c28a15ea024108c048f815120
Louis J <louis.bs.jeanneau@gmail.com>
2023-12-18 02:29:17 +0100

    2023 day 15 part 2

--- Python/2023/2023_day15.py
+++ Python/2023/2023_day15.py
@@ -4,7 +4,7 @@ with open("2023_day15_input.txt") as f:
 
 
 
-# print("input = ", data)
+print("input = ", data)
 
 # Part 1
 def hash(s):
@@ -19,6 +19,36 @@ print(sum([hash(l) for l in data[0].split(",")]))
 
 
 # Part 2
+label = ""
+lensNumber = 0
+boxes = dict()
 for operations in data[0].split(","):
-    print(operations)
-    # label =
+    # print(operations)
+    if '-' in operations:
+        label = operations.split("-")[0]
+        h = hash(label)
+        box = boxes.get(h, [])
+        for lens in box:
+            if lens[0] == label:
+                box.remove(lens)
+        boxes[h] = box
+    else:
+        label, lensNumber = operations.split("=")
+        h = hash(label)
+        box = boxes.get(h, [])
+        skip = False
+        for index, lens in enumerate(box):
+            if lens[0] == label:
+                box[index] = (label, lensNumber)
+                skip = True
+                break
+        if not skip:
+            box.append((label, lensNumber))
+        boxes[h] = box
+    # print(label, lensNumber)
+print(boxes)
+total = 0
+for k,v in boxes.items():
+    for index, box in enumerate(v):
+        total += (k+1)*(index+1)*int(box[1])
+print(total)
