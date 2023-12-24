67f4c91f441b1649cc82d3703bb59cc4705283aa
Louis Jeanneau <louis.bs.jeanneau@gmail.com>
2023-12-24 18:53:12 +0100

    2023 day 21 part 2

--- Python/2023/2023_day21.py
+++ Python/2023/2023_day21.py
@@ -35,70 +35,27 @@ print(len(positions))
 
 # Part 2
 print("Part 2")
-
-
-def has_cycle(lst):
-    if len(lst) < 150:
-        return False
-    tortoise = hare = 0
-    try:
-        while True:
-            tortoise += 1
-            hare += 2
-
-            if lst[tortoise] == lst[hare]:
-                return True  # Cycle detected
-
-            if hare is None or lst[hare] is None:
-                return False  # No cycle
-    except IndexError:
-        return False
-
-
 positions.clear()
 positions.add(start)
 reachable = []
-diff = []
-# maps = set()
-i = -1
-# while not has_cycle(diff):
 for i in range(330):
-    # i+=1
     oldPositions = deepcopy(positions)
     positions.clear()
     for p in oldPositions:
         for xo, yo in offsets:
             nextX = p[0] + xo
             nextY = p[1] + yo
-            # maps.add((nextX // height, nextY // width))
             if (nextX % height, nextY % width) not in rocks:
                 positions.add((nextX, nextY))
-    # print(f"i = {i} and len = {len(positions)}")
-    # reachable.append((len(positions), len(maps)))
-    # if i!=0:
-    #     diff.append(reachable[-1] - reachable[-2])
     if i == 64 or i == 64 + 131 or i == 64 + 2 * 131:
         reachable.append((i + 1, len(positions)))
 print(reachable)
-x = np.array([point[0] for point in reachable])
+y = np.array([point[1] for point in reachable])
 step = 26501365 // width
-a = (x[0] - x[1] + x[2]) / 2
-b = (-3 * x[0] + 4 * x[1] - x[2]) / 2
-c = x[0]
+a = (y[0] - 2*y[1] + y[2]) / 2
+b = (-3 * y[0] + 4 * y[1] - y[2]) / 2
+c = y[0]
 print(f"Solution : {a * (step ** 2) + b * step + c}")
 
-# y = np.array([point[1] for point in reachable])
-#
-# # Form the matrix A for the system of equations
-# A = np.vstack([x ** 2, x, np.ones(len(x))]).T
-#
-# # Solve for coefficients a, b, and c
-# coefficients = np.polyfit(x,y, 2)
-# print(coefficients)
-# # Coefficients will be in the order [a, b, c]
-# a, b, c = coefficients
-# step = 26501365
-# # print(diff)
-
 # 4010704921365 too low
-# 36695019340
+# 629720570456311
