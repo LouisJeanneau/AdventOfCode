10c9370a35e0e7adf0d677723e3f045d3945d90d
Louis Jeanneau <louis.bs.jeanneau@gmail.com>
2023-12-26 15:42:02 +0100

    2023 day 22 part 1 OPTIMISED

--- Python/2023/2023_day22.py
+++ Python/2023/2023_day22.py
@@ -1,4 +1,7 @@
+import time
+import timeit
 from copy import deepcopy
+from typing import Tuple
 
 # with open("2023_day22_sample.txt") as f:
 with open("2023_day22_input.txt") as f:
@@ -7,9 +10,9 @@ with open("2023_day22_input.txt") as f:
 # Part 1
 print("Part 1")
 data = [[[int(x) for x in coord.split(",")] for coord in line] for line in data]
-print(data)
+# print(data)
 data.sort(key=lambda x: x[0][2])
-print(data)
+# print(data)
 occupied = set()
 
 
@@ -26,42 +29,52 @@ def canFall(cubes, rocksSet: set[list[int]]):
     return True
 
 
-def interpolate(cubes: list[list[int]]) -> tuple[list[int]]:
+def interpolate(cubes: list[list[int]]) -> tuple[tuple[int, int, int], ...]:
     r = []
     for i in range(cubes[0][0], cubes[1][0] + 1):
         for j in range(cubes[0][1], cubes[1][1] + 1):
             for k in range(cubes[0][2], cubes[1][2] + 1):
-                r.append([i, j, k])
+                r.append((i, j, k))
     return tuple(r)
 
 
 # Falling
+start = time.time()
+# print("time", start)
 for line in data:
     while canFall(line, occupied):
         line = looseHeight(line)
-    tupled = tuple([tuple(i) for i in interpolate(line)])
+    tupled = interpolate(line)
     occupied.update(tupled)
-print(occupied)
+# print(occupied)
+print("time", time.time() - start)
 
 # scan
 total = 0
 for line in data:
-    c = deepcopy(occupied)
-    for p in interpolate(line):
-        if tuple(p) in c:
-            c.remove(tuple(p))
+    # c = deepcopy(occupied)
+    inter = interpolate(line)
+    for p in inter:
+        occupied.remove(p)
     flag = True
     for t in [i for i in data if i[0][2] == line[1][2] + 1]:
-        d = deepcopy(c)
-        for p in interpolate(t):
-            if tuple(p) in d:
-                d.remove(tuple(p))
-        if canFall(t, d):
-            flag = False
+        if not flag:
             break
-    print("flag", flag, "line", line)
+        interinter = interpolate(t)
+        for p in interinter:
+            occupied.remove(p)
+        if canFall(t, occupied):
+            flag = False
+        for p in interinter:
+            occupied.add(p)
+    # print("flag", flag, "line", line)
+    for p in inter:
+        occupied.add(p)
     if flag:
         total += 1
 print(total)
+print("time", time.time() - start)
 
 # Part 2
+# for line in data:
+#     c = deepcopy(occupied)
