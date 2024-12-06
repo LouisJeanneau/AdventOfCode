349a71554c219c79467232aed9770af888267108
Louis J <louis.bs.jeanneau@gmail.com>
2024-12-06 22:52:14 +0100

    2024 day 5 part 2 btw

--- Python/2024/2024_day05.py
+++ Python/2024/2024_day05.py
@@ -20,6 +20,7 @@ for line in data:
     if not first_part_input:
         manuals.append([int(x) for x in line.split(',')])
 
+correct_manuals = []
 ans = 0
 for manual in manuals:
     valid = True
@@ -30,4 +31,28 @@ for manual in manuals:
                 break
     if valid:
         ans += manual[len(manual) // 2]
+        correct_manuals.append(manual)
 print(f'Part 1: {ans}')
+
+# Part 2
+ans = 0
+for manual in correct_manuals:
+    manuals.remove(manual)
+
+for manual in manuals:
+    valid = False
+    while not valid:
+        restart = False
+        for i in range(len(manual) - 1):
+            for rule in rules:
+                if rule[0] in manual[i:i+2] and rule[1] in manual[i:i+2]:
+                    if manual[i]==rule[1] and manual[i+1]==rule[0]:
+                        manual[i], manual[i+1] = manual[i+1], manual[i]
+                        restart = True
+                        break
+            if restart:
+                break
+        if not restart:
+            valid = True
+    ans += manual[len(manual) // 2]
+print(f'Part 2: {ans}')
