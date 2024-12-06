1b07afbc03497bdfb5024e1465d908a7f011e546
Louis J <louis.bs.jeanneau@gmail.com>
2024-12-06 23:50:05 +0100

    2024 day 6 part 2

--- Python/2024/2024_day06.py
+++ Python/2024/2024_day06.py
@@ -1,4 +1,4 @@
-debug = False
+debug = True
 
 if debug:
     with open("2024_day06_sample.txt") as f:
@@ -23,6 +23,7 @@ direction_dict = {
 }
 direction = direction_dict[direction_char]
 directions_move = ((-1, 0), (0, 1), (1, 0), (0, -1))
+direction_start = direction
 
 i,j = start
 visited = set()
@@ -35,4 +36,26 @@ while 0<=i<len(data) and 0<=j<len(data[0]):
         i, j = ni, nj
 print(f'Part 1: {len(visited)}')
 
-
+# Part 2
+answer = 0
+visited.remove(start)
+for potentiel_obstacle in visited:
+    i, j = start
+    is_loop = False
+    new_visited = set()
+    new_obstacles = obstacle.copy()
+    new_obstacles.add(potentiel_obstacle)
+    direction = direction_start
+    while 0 <= i < len(data) and 0 <= j < len(data[0]) and not is_loop:
+        if (i,j, direction) in new_visited:
+            is_loop = True
+            break
+        new_visited.add((i, j, direction))
+        ni, nj = i + directions_move[direction][0], j + directions_move[direction][1]
+        if (ni, nj) in new_obstacles:
+            direction = (direction + 1) % 4
+        else:
+            i, j = ni, nj
+    if is_loop:
+        answer += 1
+print(f'Part 2: {answer}')
