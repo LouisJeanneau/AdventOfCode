52b4017a62385706a4d93f2c84020f75c8729d1d
Louis J <louis.bs.jeanneau@gmail.com>
2025-12-09 01:30:39 +0100

    2025 day 6 part 2 dirty numpy swapaxes

--- Python/2025/2025_day06.py
+++ Python/2025/2025_day06.py
@@ -1,12 +1,14 @@
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 debug = False
 
 if debug:
-    with open("2025/2025_day06_sample.txt") as f:
-        data = [l.strip() for l in f.readlines()]
+    with open(f'{dir_path}/2025_day06_sample.txt') as f:
+        data = [l.strip('\n') for l in f.readlines()]
 else:
-    with open("2025/2025_day06_input.txt") as f:
-        data = [l.strip() for l in f.readlines()]
+    with open(f'{dir_path}/2025_day06_input.txt') as f:
+        data = [l.strip('\n') for l in f.readlines()]
 
 # Part 1
 answer = 0
@@ -22,5 +24,28 @@ for column_index in range(len(data_splitted[0])):
             column_value *= int(data_splitted[index][column_index])
     answer += column_value
 print(f'Part 1 : {answer}')
-# 4487289 too low
 
+# Part 2
+import numpy
+answer = 0
+step_value = 0
+operator = ' '
+data_matrix = numpy.swapaxes([list(line) for line in data], 0, 1)
+for line_array in data_matrix:
+    if len(''.join(line_array).strip()) == 0:
+        answer += step_value
+        operator = ' '
+        continue
+    else:
+        line_value = int(''.join(line_array[:-1]))
+        if operator == ' ':
+            operator = line_array[-1]
+            step_value = line_value
+        else:
+            if operator == '*':
+                step_value = step_value * line_value
+            elif operator == '+':
+                step_value = step_value + line_value
+# Don't forget the last calculation step into the total
+answer += step_value
+print(f'Part 2 : {answer}')
