25380e8ff56a4c9c3e5f06bd637cead1949ac9e5
Louis Jeanneau <louis.bs.jeanneau@gmail.com>
2025-12-25 12:55:39 +0100

    2025 day 10 part 2 is easier then part 1

--- Python/2025/2025_day10.py
+++ Python/2025/2025_day10.py
@@ -16,6 +16,7 @@ else:
 # Part 1
 answer = 0
 for line in data:
+    print("#", sep='', end='')
     goal = [1 if i=='#' else 0 for i in line.split("] ")[0][1:]]
     goal = np.array(goal)
     buttons = [list(map(int, i.strip("()").split(','))) for i in line.split("] ")[1].split(' ')[:-1]]
@@ -52,6 +53,34 @@ print(f'Part 1 : {int(answer)}')
 
 # Part 2
 answer = 0
+for line in data:
+    print("#", sep='', end='')
+    goal = [int(i) for i in line.split(") {")[1][:-1].split(",")]
+    goal = np.array(goal)
+    buttons = [list(map(int, i.strip("()").split(','))) for i in line.split("] ")[1].split(' ')[:-1]]
 
+    # coefficients of the variables we want to minimize
+    c = np.ones(len(buttons))
 
-print(f'Part 2 : {answer}')
+    # constraints (A @ x = b)
+    A = np.zeros((len(goal), len(c)))
+    for col, button_group in enumerate(buttons):
+        for button in button_group:
+            A[button, col] = 1
+    constraints = LinearConstraint(A, lb=goal, ub=goal)
+    
+    # Integrality (1)
+    integrality = np.full_like(c, 1)
+
+    # Bounds (positivity)
+    bounds = Bounds(lb=0, ub=np.inf)
+
+    # Solve
+    res = milp(c=c, constraints=constraints, integrality=integrality, bounds=bounds)
+
+    if res.success:
+        answer += res.fun
+    else:
+        print("Nooooooooooooo")
+print()
+print(f'Part 2 : {int(answer)}')
